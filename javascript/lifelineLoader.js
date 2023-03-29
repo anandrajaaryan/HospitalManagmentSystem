d239316402197ad51e3c215df1c904aa7e1c3469
anandrajaaryan <anandrajaaryan@gmail.com>
2023-03-29 23:36:29 +0530

    hospital

---
+++ javascript/lifelineLoader.js
@@ -0,0 +1,114 @@
+let slideBox = document.querySelector('.slide-box');
+let solid = document.querySelector('.solid');
+
+let width = 100;
+
+let slide = setInterval(() => {
+    solid.style.width = `${width}%`;
+    width -= 1;
+}, 20)
+
+setTimeout(() => {
+    clearInterval(slide);
+    solid.style.width = `100%`;
+}, 2050);
+
+setInterval(() => {
+
+    width = 100;
+    let slide = setInterval(() => {
+        solid.style.width = `${width}%`;
+        width -= 1;
+    }, 20)
+
+    setTimeout(() => {
+        clearInterval(slide);
+    }, 2050);
+
+}, 2050);
+
+
+
+// Active cases js
+const element = document.querySelector("#container");
+
+element.addEventListener('wheel', (event) => {
+  event.preventDefault();
+
+  element.scrollBy({
+    left: event.deltaY < 0 ? -30 : 30,
+    
+  });
+});
+
+
+
+// ratings js
+
+let rateBox = Array.from(document.querySelectorAll(".rate-box"));
+let globalValue = document.querySelector(".global-value");
+let two = document.querySelector(".two");
+let totalReviews = document.querySelector(".total-reviews");
+let reviews = {
+  5: 900,
+  4: 0,
+  3: 55,
+  2: 0,
+  1: 2,
+};
+updateValues();
+
+function updateValues() {
+  rateBox.forEach((box) => {
+    let valueBox = rateBox[rateBox.indexOf(box)].querySelector(".value");
+    let countBox = rateBox[rateBox.indexOf(box)].querySelector(".count");
+    let progress = rateBox[rateBox.indexOf(box)].querySelector(".progress");
+    console.log(typeof reviews[valueBox.innerHTML]);
+    countBox.innerHTML = nFormat(reviews[valueBox.innerHTML]);
+
+    let progressValue = Math.round(
+      (reviews[valueBox.innerHTML] / getTotal(reviews)) * 100
+    );
+    progress.style.width = `${progressValue}%`;
+  });
+  totalReviews.innerHTML = getTotal(reviews);
+  finalRating();
+}
+function getTotal(reviews) {
+  return Object.values(reviews).reduce((a, b) => a + b);
+}
+
+document.querySelectorAll(".value").forEach((element) => {
+  element.addEventListener("click", () => {
+    let target = element.innerHTML;
+    reviews[target] += 1;
+    updateValues();
+  });
+});
+
+function finalRating() {
+  let final = Object.entries(reviews)
+    .map((val) => val[0] * val[1])
+    .reduce((a, b) => a + b);
+    // console.log(typeof parseFloat(final / getTotal(reviews)).toFixed(1));
+  let ratingValue = nFormat(parseFloat(final / getTotal(reviews)).toFixed(1));
+  globalValue.innerHTML = ratingValue;
+  two.style.background = `linear-gradient(to right, #66bb6a ${
+    (ratingValue / 5) * 100
+  }%, transparent 0%)`;
+}
+
+function nFormat(number) {
+  if (number >= 1000 && number < 1000000) {
+    return `${number.toString().slice(0, -3)}k`;
+  } else if (number >= 1000000 && number < 1000000000) {
+    return `${number.toString().slice(0, -6)}m`;
+  } else if (number >= 1000000000) {
+    return `${number.toString().slice(0, -9)}md`;
+  } else if (number === "NaN") {
+    return `0.0`;
+  } else {
+    return number;
+  }
+}
+
